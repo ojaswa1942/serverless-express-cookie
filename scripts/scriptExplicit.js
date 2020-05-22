137437b0d7893ffad0cb16ad9b670241928d8f29
Ojaswa Sharma <ojaswa1942@gmail.com>
2020-05-22 20:53:41 +0530

    Update randomeBinary

--- scripts/scriptExplicit.js
+++ scripts/scriptExplicit.js
@@ -1,4 +1,4 @@
 console.log('I am a script which sets cookie explicitly using document.cookie');
 
-let randomBinary = Math.random().toString(2).substr(2, 50);
-document.cookie = `scriptExplicit=${randomBinary}; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT;`;
+// let randomBinary = Math.random().toString(2).substr(2, 50);
+document.cookie = `scriptExplicit=${Math.random().toString(2).substr(2, 50)}; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT;`;
--- scripts/scriptHeaderAndExplicit.js
+++ scripts/scriptHeaderAndExplicit.js
@@ -1,4 +1,4 @@
 console.log('I am a script which sets both Set-Cookie Header AND cookie explicitly using document.cookie');
 
-let randomBinary = Math.random().toString(2).substr(2, 50);
-document.cookie = `explicit_scriptHeaderAndExplicit=${randomBinary}; path=/; expires=Tue, 21 Jan 2038 03:14:07 GMT;`;
+// let randomBinary = Math.random().toString(2).substr(2, 50);
+document.cookie = `explicit_scriptHeaderAndExplicit=${Math.random().toString(2).substr(2, 50)}; path=/; expires=Tue, 21 Jan 2038 03:14:07 GMT;`;
