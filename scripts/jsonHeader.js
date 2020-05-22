0b3b8065ef65249b23fe35e4fc288ae7dab49fd5
Ojaswa Sharma <ojaswa1942@gmail.com>
2020-05-22 21:26:31 +0530

    uncertain changes

--- scripts/jsonHeader.js
+++ scripts/jsonHeader.js
@@ -1,5 +1,8 @@
 console.log(`This is jsonHeader.js: Fetch request to receive a JSON with Set-Cookie`);
 
-fetch('https://cookietest-domain1.netlify.app/.netlify/functions/server/jsonHeader')
+fetch('https://cookietest-domain1.netlify.app/.netlify/functions/server/jsonHeader', {
+    method: 'GET',
+    credentials: 'include'
+})
 .then(res => res.json())
 .then(console.log);
